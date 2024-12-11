73724e22fd61b0ff0cb18a2a7b87015662777a34
mythemeria <mythemeria@users.noreply.github.com>
2024-12-11 21:59:09 +1100

    minor improvements on day 11 solution

--- 2024/11/aoc11.py
+++ 2024/11/aoc11.py
@@ -1,39 +1,40 @@
-n2 = ['510613', '358', '84', '40702', '4373582', '2', '0', '1584']
+from time import perf_counter_ns
+from functools import cache
 
+n2 = [510613, 358, 84, 40702, 4373582, 2, 0, 1584]
+
+@cache
 def doBlink(item):
-  if item == "0":
-    return "1"
+  if not item:
+    return 1
   else:
-    length = len(item)
+    asStr = f'{item}'
+    length = len(asStr)
     if length % 2 == 0:
       length //= 2
-      return [item[:length], item[length:].lstrip("0") or "0"]
+      return [int(asStr[:length]), int(asStr[length:].lstrip("0") or 0)]
     else:
-      return str(int(item) * 2024)
+      return item * 2024
 
-lookup = {}
-
-def blink(num, count, remaining):
-  if num not in lookup:
-    lookup[num] = {}
-  
-  cached = lookup[num].get(remaining)
-  if cached is not None:
-    return cached
-
+@cache
+def blink(num, remaining):
   item = doBlink(num)
   if remaining == 0:
     n = 2 if isinstance(item, list) else 1
-    lookup[num][remaining] = n
     return n
 
   if isinstance(item, list):
-    child = blink(item[0], count, remaining - 1) + blink(item[1], count, remaining - 1)
+    child = blink(item[0], remaining - 1) + blink(item[1], remaining - 1)
   else:
-    child = blink(item, count, remaining - 1)
+    child = blink(item, remaining - 1)
 
-  lookup[num][remaining] = child
-  return child + count
+  return child
 
-for i in range(75):
-  print(i + 1, sum([blink(x, 0, i) for x in n2]))
+i = 30
+start = perf_counter_ns()
+rocks = sum([blink(x, i) for x in n2])
+end = perf_counter_ns()
+
+print(f'f({i + 1}) = {rocks} mine: {end - start} ns')
+print(blink.cache_info())
+print(doBlink.cache_info())
